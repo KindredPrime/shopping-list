167636cf314f71408f5c68ef3af07ded492c85ce
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-04-20 15:46:43 -0400

    added add item feature

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 $(function() {
+    $("form").submit(function(event) {
+        event.preventDefault();
+        const item = $(this).find("input[name='shopping-list-entry']").val();
+
+        $(".shopping-list").append(`<li><span class=\"shopping-item\">${item}</span><div class=\"shopping-item-controls\"><button class=shopping-item-toggle\"><span class=\"button-label\">check</span></button><button class=\"shopping-item-delete\"><span class=\"button-label\">delete</span></button></div></li>`);
+    });
+
     $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
         $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
     });
