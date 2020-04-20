a22970538b50e681a3f0440a4d41f0bdb58ed282
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-04-20 17:59:49 -0400

    fixed a typo in the added items that kept the check feature from working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(function() {
         event.preventDefault();
         const item = $(this).find("input[name='shopping-list-entry']").val();
 
-        $(".shopping-list").append(`<li><span class=\"shopping-item\">${item}</span><div class=\"shopping-item-controls\"><button class=shopping-item-toggle\"><span class=\"button-label\">check</span></button><button class=\"shopping-item-delete\"><span class=\"button-label\">delete</span></button></div></li>`);
+        $(".shopping-list").append(`<li><span class="shopping-item">${item}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
     });
 
     $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
