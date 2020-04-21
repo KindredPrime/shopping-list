e123a6d5b9cbc74fecf75a5438d38fa98a5f5e69
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-04-20 21:30:15 -0400

    Added comments for the add and check features in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(function() {
+    // Event listener that adds a new shopping item when the "Add item" button is selected
     $("form").submit(function(event) {
         event.preventDefault();
         const item = $(this).find("input[name='shopping-list-entry']").val();
@@ -6,6 +7,8 @@ $(function() {
         $(".shopping-list").append(`<li><span class="shopping-item">${item}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
     });
 
+    // Event delegation that adds a line-through style to the shopping item when the "check"
+    // button is selected
     $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
         $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
     });
